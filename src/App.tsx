a9e6fb0ae0bbe194b776130f0270a9d286e58318
Joao Textor <joaotextor@gmail.com>
2023-02-17 09:10:37 -0300

    Put components in app and import bootstrap icons

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,52 @@ import Header from './components/Header'
 import styles from './App.module.css'
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
+import Modal from './components/Modal'
 
 import {ITask} from './interfaces/Task'
 
 function App() {
+
+  const [taskList, setTaskList] = React.useState<ITask[]>([])
+
+  const deleteTask = (id: number) => {
+    setTaskList(
+      taskList.filter(task => {
+        return task.id !== id
+      })
+    )
+  }
+
+  const modalHideShow = (display: boolean) => {
+    const modal = document.querySelector('#modal')
+    if(display) {
+      modal!.classList.remove("hide")
+    } else {
+      modal!.classList.add("hide")
+    }
+  }
+
+  const editTask = () => {
+    modalHideShow(true)
+  }
+
   return (
     <div>
+    <Modal children={
+      <TaskForm
+        btnText="Edit Task"
+        taskList={taskList}
+        setTaskList={setTaskList}
+      />
+    }/>
     <Header/>
       <main className={styles.main}>
         <div className="">
-          <h2>O que você vai fazer?</h2>
-          <TaskForm btnText="Create Task"/>
+          <h2>What do you plan to do?</h2>
+          <TaskForm btnText="Create Task" taskList={taskList} setTaskList={setTaskList}/>
         </div>
         <div className="">
-          <TaskList/>
+          <TaskList taskList={taskList} handleDelete={deleteTask} handleEdit={editTask}/>
         </div>
       </main>
     <Footer/>
