2908c773cc6a9445297245a0bda953d2abe1d176
Joao Textor <joaotextor@gmail.com>
2023-02-17 09:10:19 -0300

    Create task list to show tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,13 +1,38 @@
 import * as React from 'react';
 
+import { ITask } from '../interfaces/Task'
+
+import styles from './TaskList.module.css'
+
+
 export interface ITaskListProps {
+  taskList: ITask[],
+  handleDelete(id: number): void
+  handleEdit(): void
 }
 
-export default function TaskList (props: ITaskListProps) {
+export default function TaskList ({taskList, handleDelete, handleEdit}: ITaskListProps) {
   return (
-    <div>
-        <h2>Lista de Tarefas</h2>
-    </div>
+    <>
+      {taskList.length > 0 
+      ? (
+        taskList.map(task => (
+          <div key={task.id} className={styles.task}>
+            <div className={styles.details}>
+              <h4>{task.title}</h4>
+              <p>Difficulty: {task.difficulty}</p>
+            </div>
+            <div className={styles.actions}>
+              <i className="bi bi-pencil" onClick={() => handleEdit()}></i>
+              <i className="bi bi-trash" onClick={() => handleDelete(task.id)}></i>
+            </div>
+          </div>
+        ))
+      )
+      : <p>Yay! No tasks to do.</p>
+      }
+       
+    </>
   );
 }
 
